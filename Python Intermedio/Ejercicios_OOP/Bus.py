29ee7f8c38ec5c0bc8fb48b908f0679a475fd3f8
Jose Herrera <josexd96@gmail.com>
2026-07-27 20:11:52 -0600

    1st_request_changes

--- Python Intermedio/Ejercicios_OOP/Bus.py
+++ Python Intermedio/Ejercicios_OOP/Bus.py
@@ -1,18 +1,18 @@
-from person import person
+from Person import Person
 
 class Bus:
-    passengers
     def __init__(self, max_passengers):
         self.max_passengers = max_passengers
+        self.passengers = []
     
-    def add_passenger(self, passenger: person):
+    def add_passenger(self, passenger: Person):
         if len(self.passengers) >= self.max_passengers:
             print("Bus is full")
             return
         self.passengers.append(passenger)
         print(f"{passenger.name} added to bus")
 
-    def remove_passenger(self, passenger: person):
+    def remove_passenger(self, passenger: Person):
         if passenger in self.passengers:
             self.passengers.remove(passenger)
             print(f"{passenger.name} removed from bus")
