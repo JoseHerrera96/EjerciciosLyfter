0d49b313369b5573d1e8f15bbfc2204a178e9259
Jose Herrera <josexd96@gmail.com>
2026-08-16 02:27:00 -0600

    4-pilares-Extra-inheritance-cambios

--- Python Intermedio/Ejercicios_4_Pilares_OOP_extra/Employee.py
+++ Python Intermedio/Ejercicios_4_Pilares_OOP_extra/Employee.py
@@ -1,21 +1,25 @@
 class Employee:
     def __init__(self, name, salary):
         self._name = name
-        self._salary = salary
+        self.set_salary(salary)
 
     def promote(self, raise_amount):
-        self._salary += raise_amount
+        self._salary += raise_amount*self._salary
     
     @property
-    def Employee(self):
-        return self._name, self._salary
+    def name(self):
+        return self._name
     
-    @Employee.setter
-    def Employee(self, new_name):
-        if salary > 0:
-            self._name = new_name
-            self._salary = salary
+    @property
+    def salary(self):
+        return self._salary
+
+    
+    @salary.setter
+    def set_salary(self, new_salary):
+        if new_salary > 0:
+            self._salary = new_salary
         else:
-            print("El salary must be be positive")
+            print("The salary must be be positive")
 
 
