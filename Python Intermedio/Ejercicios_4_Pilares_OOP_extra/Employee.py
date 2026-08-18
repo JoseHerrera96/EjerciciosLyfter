c9d9347ff1fa08bb7e69aa050071639ea93adbe1
Jose Herrera <josexd96@gmail.com>
2026-08-18 12:47:08 -0600

    4-pilares-Extra-cambios

--- Python Intermedio/Ejercicios_4_Pilares_OOP_extra/Employee.py
+++ Python Intermedio/Ejercicios_4_Pilares_OOP_extra/Employee.py
@@ -1,7 +1,7 @@
 class Employee:
     def __init__(self, name, salary):
         self._name = name
-        self.set_salary(salary)
+        self.salary = salary
 
     def promote(self, raise_amount):
         self._salary += raise_amount*self._salary
@@ -16,7 +16,7 @@ class Employee:
 
     
     @salary.setter
-    def set_salary(self, new_salary):
+    def salary(self, new_salary):
         if new_salary > 0:
             self._salary = new_salary
         else:
