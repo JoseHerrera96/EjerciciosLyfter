7afb4c96df682e21e2f00e146ee490822ddc8923
Jose Herrera <josexd96@gmail.com>
2026-09-13 00:35:07 -0600

    Decorators extra-exercises, changes 1

--- Python Intermedio/Decorators_extra/operation_decorators.py
+++ Python Intermedio/Decorators_extra/operation_decorators.py
@@ -1,14 +1,10 @@
 from functools import wraps
-
-
-def multiply(A, B):
-    return A * B
-    
+from datetime import datetime
 
 def log_call(func):
-    @wraps
+    @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"Calling function {func.__name__} with arguments {args} and keyword arguments {kwargs}")
+        print(f"Calling function {func.__name__} with arguments {args} and keyword arguments {kwargs} at {datetime.now()}")
         result = func(*args, **kwargs)
         print(f"Function {func.__name__} returned {result}")
         return result
@@ -24,3 +20,9 @@ def validate_numbers(func):
             print(f"Result: {result}")
             return result
     return wrapper
+
+
+@log_call
+@validate_numbers
+def multiply(A, B):
+    return A * B
