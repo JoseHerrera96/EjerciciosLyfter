18b7e740ecda4070125a79ac9ee571b89ee38b79
Jose Herrera <josexd96@gmail.com>
2026-09-15 21:19:16 -0600

    Decorators extra-exercises, changes 2

--- Python Intermedio/Decorators_extra/requires_log_decorator.py
+++ Python Intermedio/Decorators_extra/requires_log_decorator.py
@@ -2,13 +2,14 @@
 from functools import wraps
 
 global user_logged_in
-user_logged_in : bool 
+user_logged_in : bool = False
 
 def requires_log_decorator(func):
     @wraps(func)
-    def wrapper():
+    def wrapper(*args, **kwargs):
         if not user_logged_in:
-            raise ValueError("User isn't authenticated")
+            raise ValueError("Usuario no autenticado")
         else:
-            return func(user_logged_in)
+            result = func(*args, **kwargs)
+            return result
     return wrapper
